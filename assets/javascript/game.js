6b6027e6b86804f74a37e4395ba8b1f030d1f8a4
Denise Middleton <demiddleton@bellsouth.net>
2019-07-29 18:31:42 -0400

    cut javascript code from the html file and pasted it in the game.js file

---
+++ assets/javascript/game.js
@@ -0,0 +1,44 @@
+
+// Creates an array that lists out all of the options).
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h ", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+// Declaring variables to track wins and losses
+var wins = 0;
+var losses = 0;
+var guesses = 10;
+
+
+// Create variables that hold references to the places in the HTML to display the outcome.
+
+var userChoiceText = document.getElementById("userchoice-text");
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
+var guessesText = document.getElementById("guesses-text");
+
+
+// This function is run whenever the user presses a key.
+document.onkeyup = function (event) {
+
+    var userGuess = event.key;
+
+    var computerGuess = computerChoices[Math.floor(Math.random())];
+    console.log(computerGuess);
+
+    if ((userGuess === computerGuess)) {
+        wins++;
+    } else {
+        losses++;
+    }            
+    guesses--;
+
+    if (guesses < 0) {
+        return;
+    }
+
+    // Display the user and computer guesses, and wins/losses/ties.
+    userChoiceText.textContent = userGuess;
+    winsText.textContent = wins;
+    lossesText.textContent = losses;
+    guessesText.textContent = guesses;
+
+};
